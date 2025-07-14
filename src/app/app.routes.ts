b961d0657946d3ca890978f18112f33230e7e78a
mSev7n <pythagorasm7@gmail.com>
2025-07-14 14:15:50 +0100

    created standalone component and placed the files directly inside src/app/pages/, edited app.route.ts to show HomeComponents when user visits and redirect any unknown routes to the homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''  // fallback for unknown routes to redirect to homepage
+  }
+];
