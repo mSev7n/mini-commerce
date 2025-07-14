8b18feb02daed07194fababdd3ce8140b173e4fb
mSev7n <pythagorasm7@gmail.com>
2025-07-14 16:02:32 +0100

    Fetch and show products on homepage using ProductService

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ProductService, Product } from '../services/product.service';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  loading = true;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    // Call service to get products and show loading state while fetching
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Failed to load products:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
