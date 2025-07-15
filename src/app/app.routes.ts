ab0a5bbf6f4f45bd4a91563a04d75c613de758c9
mSev7n <pythagorasm7@gmail.com>
2025-07-15 09:26:02 +0100

    added new route for product slug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home.component';
+import { ProductDetailComponent } from './pages/product-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  { path: 'product/:slug',
+    component: ProductDetailComponent 
   },
   {
     path: '**',
